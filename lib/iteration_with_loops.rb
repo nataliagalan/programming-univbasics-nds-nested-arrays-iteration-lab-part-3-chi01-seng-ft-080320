a268713e7d7e3820baf10bc94ae572a0e93f3449
Natalia Galan <n8galan@gmail.com>
2020-07-03 03:15:15 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,8 +6,8 @@ def join_nested_strings(src)
   #flat_map Returns a new array with the concatenated results of running block once for every element in enum.
   sentence = src.flat_map do |elements|
     elements.select { |element| element.class == String }
-    binding.pry
+    
   end.join(" ")
-
+binding.pry
   sentence
 end
